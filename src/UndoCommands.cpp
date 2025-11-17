8ae0ebb0ee9714f50fe96c5f9d1f209eb0cdcd39
Cal Verah <Calaverahmedia@gmail.com>
2025-11-17 15:31:42 -0600

    uint fix on serialization

--- src/UndoCommands.cpp
+++ src/UndoCommands.cpp
@@ -63,7 +63,7 @@ static void insertSerializedItems(QJsonObject const &json, BasicGraphicsScene *s
 
         graphModel.loadNode(obj);
 
-        auto id = obj["id"].toInt();
+        auto id = obj["id"].toString().toUInt();
         scene->nodeGraphicsObject(id)->setZValue(1.0);
         scene->nodeGraphicsObject(id)->setSelected(true);
     }
@@ -98,7 +98,7 @@ static void deleteSerializedItems(QJsonObject &sceneJson, AbstractGraphModel &gr
 
     for (QJsonValueRef node : nodesJsonArray) {
         QJsonObject nodeJson = node.toObject();
-        graphModel.deleteNode(nodeJson["id"].toInt());
+        graphModel.deleteNode(nodeJson["id"].toString().toUInt());
     }
 }
 
@@ -332,7 +332,7 @@ QJsonObject PasteCommand::makeNewNodeIdsInScene(QJsonObject const &sceneJson)
     for (QJsonValueRef node : nodesJsonArray) {
         QJsonObject nodeJson = node.toObject();
 
-        NodeId oldNodeId = nodeJson["id"].toInt();
+        NodeId oldNodeId = nodeJson["id"].toString().toUInt();
 
         NodeId newNodeId = graphModel.newNodeId();
 
